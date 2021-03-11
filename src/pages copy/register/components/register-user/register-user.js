86d4be3cc0ade16f4755ca14ebbae1cb80460c77
Sara <sarisilvolopez1991@gmail.com>
2021-03-12 00:55:49 +0100

    css register

--- src/pages copy/register/components/register-user/register-user.js
+++ src/pages copy/register/components/register-user/register-user.js
@@ -1,19 +1,41 @@
 import React from "react"
 import css from "./register-user.module.css"
-import {Input, Button, Form} from '../../../../components'
+import { Input, Button, Form, useForm, FormGroup, FormControl } from '../../../../components'
 
-export function RegisterUser (){
+
+export function RegisterUser() {
+    const frm = new FormGroup({
+        "email": new FormControl(),
+        "name": new FormControl(),
+        "surname": new FormControl(),
+        "password": new FormControl(),
+        "privacy": new FormControl(),
+        "accept": new FormControl(),
+    });
+    const {
+        register, handlerSubmit
+    } = useForm(frm);
+    function submit(data) {
+        console.log(data)
+    }
     return (
         <div className={css.registeruser}>
-        <h1>¿Quieres registrarte?</h1>
-        <Form>
-            <Input className={css.container} placeholder="Tu e-mail" />
-            <Input className={css.container} placeholder="Nombre" />
-            <Input className={css.container} placeholder="Apellidos"/>
-            <Input  className={css.container} placeholder="Contraseña"/>
-            <Input type="checkbox" defaultChecked/> <p>"He leído y acepto la Política de Privacidad"</p> 
-            <Input type="checkbox" defaultChecked/> <p>"Sí,acepto recibir comunicaciones comerciales y ofertas personalizadas según mi perfil"</p>
-            <button>Registrarme</button>
-        </Form>
+            <h1>¿Quieres registrarte?</h1>
+            <Form onSubmit={handlerSubmit(submit)}>
+                <Input className={css.container} placeholder="Tu e-mail" ref={register(frm.email)} />
+                <Input className={css.container} placeholder="Nombre"  ref={register(frm.name)}/>
+                <Input className={css.container} placeholder="Apellidos" ref={register(frm.surname)} />
+                <Input className={css.container} type="password" placeholder="Contraseña" ref={register(frm.password)} />
+                <div className={css.privacy}>
+                    <Input type="checkbox" defaultChecked  ref={register(frm.privacy)}/>
+                    <p>"He leído y acepto la Política de Privacidad" </p>
+                </div>
+                <div className={css.accept}>
+                <Input type="checkbox" defaultChecked  ref={register(frm.accept)}/>
+                <p>"Sí,acepto recibir comunicaciones comerciales y ofertas personalizadas según mi perfil"</p>
+                </div>
+                <button type="submit">Registrarme</button>
+            </Form>
         </div>
-        )}
+    )
+}
