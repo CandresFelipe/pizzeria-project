168bc4650cdcccc4fcab64cdc42e966a9658adc0
Andres Carrera <carrerafandres2496gmail.com>
2021-03-17 21:19:00 -0500

    missing fix button from pizza-detail

--- src/components/ingredient-list/ingredient-item.js
+++ src/components/ingredient-list/ingredient-item.js
@@ -1,10 +1,12 @@
 import React from "react"
 import css from './ingredient-item.module.css'
 
-export function IngredientItem({ ingredient }) {
+export function IngredientItem({ price, name }) {
+    const totalIng = price && `${name} (${price}€)` || name && `${name}`;
     return (
         <div className={css.ingredientItem}>
-            {ingredient.name} ({ingredient.price}€)
+        {totalIng}
         </div>
+
     )
 }
--- src/pages/pizzas-detail/index.js
+++ src/pages/pizzas-detail/index.js
@@ -5,21 +5,22 @@ import { PizzaDetails } from './components/pizza-details';
 import { PizzaComments } from './components/pizza-comments/pizza-comments'
 import { CommentCreation } from './components/pizza-form-comment/create-comment'
 import css from './pizza-detail.module.css'
+import '../../styles'
 
 export default function PizzaDetail({ pizza }) {
     const items = [{ text: "Carta", url: "/" }, { text: pizza.name }]
     return (
 
         <Page>
-            <main>
+            <main className={css.main}>
                 <h1 className="visually-hidden">Detalles de la pizza</h1>
                 <BreadCrumbs items={items} />
                 <div>
-                    <Image public_id={pizza.image.public_id} width={5} height={4} big={1024} small={50} />
+                    <Image public_id={pizza.image.public_id} width={16} height={9} big={1024} small={500} />
                 </div>
                 <PizzaDetails pizza={pizza} />
+                <h2>Ingredientes</h2>
                 <div>
-                    <h2>Ingredientes</h2>
                     <IngredientList ingredients={pizza.ingredients} />
                     <div className={css.cartForm}>
                         <CartForm id={pizza.id} />
