29a1236c25850809690cc9151591e00fc91824f6
Sara <sarisilvolopez1991@gmail.com>
2021-03-18 20:55:20 +0100

    css Ingredients done is done

--- src/pages/ingredient/index.js
+++ src/pages/ingredient/index.js
@@ -8,7 +8,7 @@ import '../../styles'
 export default function Ingredients({ ingredients }) {
     return (
         <Page>
-            <main>
+            <main className={css.pagina}>
                 <h1 className= "visually-hidden">Gestionar Ingredientes</h1>
                 <h2>Todos los ingredientes</h2>
                 <IngredientList ingredients={ingredients} />
