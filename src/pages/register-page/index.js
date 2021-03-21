174a2f181bc1dbda10de7d050394a6d02fe67d8e
Sara <sarisilvolopez1991@gmail.com>
2021-03-21 18:35:58 +0100

    register funcionando

---
+++ src/pages/register-page/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {IconPage} from "../../components";
+import {RegisterUser} from './register-user'
+
+
+export default function SignUp() {
+    return (
+        <IconPage>
+            <main>
+                <RegisterUser />
+            </main>
+        </IconPage>
+    )
+}
---
+++ src/pages/register-page/page.js
@@ -0,0 +1,7 @@
+import SignUp from './index';
+
+
+SignUp.route='/signup';
+
+
+export default SignUp; 
